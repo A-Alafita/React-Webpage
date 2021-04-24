37f49b6bb29897c6669efa0153f0f72c411ce6cf
Victor <A00827302@itesm.mx>
2021-04-24 18:11:06 -0500

    working on side navbar

--- src/Header/Navigation.js
+++ src/Header/Navigation.js
@@ -1,20 +1,56 @@
 import AccountBoxIcon from '@material-ui/icons/AccountBox'
+import DashBoard from '@material-ui/icons/Dashboard'
 import '../CSS/Nav.css'
-import { Navbar, Nav} from 'react-bootstrap'
+import { Navbar, Nav, Tab, Row, Col } from 'react-bootstrap'
 const Navigation = () => {
     return (
-        <Navbar bg="dark" variant='dark' expand='lg'>
-            <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-            <Navbar.Collapse id="basic-navbar-nav" className="navbarID">
-                <div className = "navbar-style">
-                <Nav className="mr-auto flex-column options">
-                    <Nav.Link aria-current href='/Portal'>Home</Nav.Link>
-                    <Nav.Link aria-current href='/'>Testing</Nav.Link>
-                    <Nav.Link style = {{color:"white", paddingLeft:'25px'}}><AccountBoxIcon fontSize = 'large' style = {{color:'white', paddingLeft:'10px'}}/>User Profile</Nav.Link>
-                </Nav>
-                </div>
-            </Navbar.Collapse>
-        </Navbar>
+        <div className='sideNav'>
+            <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+                <Row>
+                    <Col lg={3}>
+                        <Nav variant="pills" className="flex-column">
+                            <Nav.Item>
+                                <Nav.Link aria-current href='/Portal' className='' ><DashBoard fontSize='large' />Home</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link aria-current href='/' className=''>Testing</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link><AccountBoxIcon fontSize='large' className='' />User Profile</Nav.Link>
+                            </Nav.Item>
+                        </Nav>
+                    </Col>
+                    <Col lg={9}>
+                        <Tab.Content>
+                            <Tab.Pane eventKey="first">
+                                
+                            </Tab.Pane>
+                            <Tab.Pane eventKey="second">
+                            </Tab.Pane>
+                        </Tab.Content>
+                    </Col>
+                </Row>
+            </Tab.Container>
+        </div>
+        //!working navbar but no tell of which page you're on 
+        // <Navbar bg="default" variant='dark' expand='lg' className='sideNav'>
+        //     <Navbar.Toggle aria-controls='responsive-navbar-nav' className="optionBox" />
+        //     <Navbar.Collapse id="basic-navbar-nav" className="optionBox">
+        //         <div className="navbar-style">
+        //             <Nav variant="pills" className= "flex-column " style={{ paddingBottom: '20px' }}>
+        //                 <Nav.Item>
+        //                     <Nav.Link aria-current href='/Portal' className='' ><DashBoard fontSize='large' />Home</Nav.Link>
+        //                 </Nav.Item>
+        //                 <Nav.Item>
+        //                     <Nav.Link aria-current href='/' className=''>Testing</Nav.Link>
+        //                 </Nav.Item>
+        //                 <Nav.Item>
+        //                     <Nav.Link><AccountBoxIcon fontSize='large' className='' />User Profile</Nav.Link>
+        //                 </Nav.Item>
+        //             </Nav>
+        //         </div>
+        //     </Navbar.Collapse>
+        // </Navbar>
     );
 }
 
--- src/Pages/Portal.js
+++ src/Pages/Portal.js
@@ -4,9 +4,7 @@ import Header from '../Header/Header'
 function Portal() {
     return (
         <div>
-            <div className ='sideNav'>
-            <Header />
-            </div>
+            <Header/>
             <div className='welcome-box'>
                 <p>Welcome</p>
             </div>
