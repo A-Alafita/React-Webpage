75c3545a5ef4aa427ed729da4211d4743c549685
Victor <A00827302@itesm.mx>
2021-04-26 11:08:38 -0500

    working on side navbar

--- src/Header/Navigation.js
+++ src/Header/Navigation.js
@@ -2,40 +2,32 @@ import AccountBoxIcon from '@material-ui/icons/AccountBox'
 import DashBoard from '@material-ui/icons/Dashboard'
 import '../CSS/Nav.css'
 import { Navbar, Nav, Tab, Row, Col } from 'react-bootstrap'
-const Navigation = () => {
+function Navigation(props) {
+
+
+
+
     return (
-        <div className='sideNav'>
-            <Tab.Container id="left-tabs-example" defaultActiveKey="first">
-                <Row>
-                    <Col lg={3}>
-                        <Nav variant="pills" className="flex-column">
-                            <Nav.Item>
-                                <Nav.Link aria-current href='/Portal' className='' ><DashBoard fontSize='large' />Home</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link aria-current href='/' className=''>Testing</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link><AccountBoxIcon fontSize='large' className='' />User Profile</Nav.Link>
-                            </Nav.Item>
-                        </Nav>
-                    </Col>
-                    <Col lg={9}>
-                        <Tab.Content>
-                            <Tab.Pane eventKey="first">
-                                
-                            </Tab.Pane>
-                            <Tab.Pane eventKey="second">
-                            </Tab.Pane>
-                        </Tab.Content>
-                    </Col>
-                </Row>
-            </Tab.Container>
-        </div>
-        //!working navbar but no tell of which page you're on 
-        // <Navbar bg="default" variant='dark' expand='lg' className='sideNav'>
-        //     <Navbar.Toggle aria-controls='responsive-navbar-nav' className="optionBox" />
-        //     <Navbar.Collapse id="basic-navbar-nav" className="optionBox">
+       <Row>
+           <Col>
+            <Nav className = "nav flex-column nav-pills" v = 'v-pills-tab' role = 'tablist' >
+                    <Nav.Link aria-current href='/Portal' className='' ><DashBoard fontSize='large' />Home</Nav.Link>
+                    <Nav.Link aria-current href='/' className=''>Testing</Nav.Link>
+                    <Nav.Link><AccountBoxIcon fontSize='large' className='' />User Profile</Nav.Link>
+            </Nav>
+
+
+           </Col>
+       </Row>
+
+
+
+
+        // //!working navbar but no tell of which page you're on 
+        // <div className = 'wrapper'>
+        // <Navbar bg="default" variant='dark' expand='lg' id = "sidebar">
+        //     <Navbar.Toggle bg ="dark"aria-controls='responsive-navbar-nav' className="optionBox" />
+        //     <Navbar.Collapse id="basic-navbar-nav" className="optionBox" id = 'Submenu'>
         //         <div className="navbar-style">
         //             <Nav variant="pills" className= "flex-column " style={{ paddingBottom: '20px' }}>
         //                 <Nav.Item>
@@ -51,6 +43,7 @@ const Navigation = () => {
         //         </div>
         //     </Navbar.Collapse>
         // </Navbar>
+        // </div>
     );
 }
 
